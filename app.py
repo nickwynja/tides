d9413f66b6c2ae5498e250fa2c620dadc5fb12e5
Nick Wynja <nick@nickwynja.com>
2024-10-25 13:47:44 -0400

    caching and wind

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ import pandas as pd
 from io import StringIO
 import plotly.express as px
 import plotly.graph_objects as go
+from requests_cache import CachedSession
+import pprint
+import math
 
 
 app = Flask(__name__)
@@ -29,6 +32,24 @@ def improve_text_position(x):
     positions = ['top center', 'bottom center']  # you can add more: left center ...
     return [positions[i % len(positions)] for i in range(len(x))]
 
+def seconds_until_hour():
+    delta = timedelta(hours=1)
+    now = datetime.now()
+    next_hour = (now + delta).replace(microsecond=0, second=0, minute=0)
+    wait_seconds = (next_hour - now).seconds
+    return wait_seconds
+
+def km_to_kn(speed):
+    return math.floor(int(speed) * 0.53996)
+
+def deg_to_compass(num):
+    num=int(num)
+    val=int((num/22.5)+.5)
+    arr=["N","NNE","NE","ENE","E","ESE", "SE", "SSE","S","SSW","SW","WSW","W","WNW","NW","NNW"]
+    return arr[(val % 16)]
+
+
+session = CachedSession()
 
 @app.route('/')
 def tides():
@@ -40,8 +61,31 @@ def tides():
     tide_station = "8512053" # Hash
     current_station = "ACT2781" # Rocky pt
 
-    tides_csv = requests.get(f"https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?product=predictions&application=NOS.COOPS.TAC.WL&begin_date={start_date}&end_date={end_date}&datum=MLLW&station={tide_station}&time_zone=lst_ldt&units=english&interval=hilo&format=csv")
-    currents_csv = requests.get(f"https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?product=currents_predictions&application=NOS.COOPS.TAC.WL&begin_date={start_date}&end_date={end_date}&datum=MLLW&station={current_station}&time_zone=lst_ldt&units=english&interval=6&format=csv")
+    cache_expires = seconds_until_hour()
+
+    forecast_hourly = session.get(
+            f"https://api.weather.gov/gridpoints/OKX/87,63",
+            expire_after=cache_expires,
+            )
+
+    forecast_daily = session.get(
+            f"https://dataservice.accuweather.com/forecasts/v1/daily/5day/2274451?apikey=qhBeSntg5AV4KEv6AbtWQOOQKVnGAkY9&details=true",
+            expire_after=cache_expires,
+            )
+
+    forecast_marine = session.get(
+            f"https://forecast.weather.gov/MapClick.php?lat=41.1328&lon=-72.6664&FcstType=digitalDWML",
+            expire_after=cache_expires,
+            )
+
+    tides_csv = session.get(
+            f"https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?product=predictions&application=NOS.COOPS.TAC.WL&begin_date={start_date}&end_date={end_date}&datum=MLLW&station={tide_station}&time_zone=lst_ldt&units=english&interval=hilo&format=csv",
+            expire_after=cache_expires,
+            )
+    currents_csv = session.get(
+            f"https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?product=currents_predictions&application=NOS.COOPS.TAC.WL&begin_date={start_date}&end_date={end_date}&datum=MLLW&station={current_station}&time_zone=lst_ldt&units=english&interval=6&format=csv",
+            expire_after=cache_expires,
+            )
 
     dt = pd.read_table(StringIO(tides_csv.text), sep=",", names=["Date", "Feet", "Type"], skiprows=1)
     dt['Date'] = pd.to_datetime(dt['Date'])
@@ -78,29 +122,70 @@ def tides():
     )
 
 
-    solar = requests.get("https://gml.noaa.gov/grad/solcalc/table.php?lat=41.130783&lon=-72.362957&year=2024")
-    dss = pd.read_html(StringIO(solar.text))
+    for d in forecast_daily.json()['DailyForecasts']:
+        fig = add_sun_annot(fig, pd.to_datetime(d['Sun']['Rise']))
+        fig = add_sun_annot(fig, pd.to_datetime(d['Sun']['Set']))
 
-    dr = dss[0]
-    ds = dss[1]
 
-    datelist = pd.date_range(datetime.today() - timedelta(days = 1), periods=DAYS).tolist()
+    import xml.etree.ElementTree as ET
 
-    sro = dr.astype(object)
-    sso = ds.astype(object)
+    tree = ET.ElementTree(ET.fromstring(forecast_marine.text))
 
-    for single_date in datelist:
-      month = single_date.strftime("%b")
-      day = single_date.strftime("%d")
+    root = tree.getroot()
 
-      sun_rise_time = sro[month][int(day)-1]
-      sun_rise = pd.to_datetime(f"{single_date.strftime('%Y-%m-%d')} {sun_rise_time}")
-      fig = add_sun_annot(fig, sun_rise)
+    times = root.findall('.//start-valid-time')
+    wind_speeds = root.findall('.//wind-speed[@type="sustained"]/value')
+    wind_dir = root.findall('.//direction[@type="wind"]/value')
 
-      sun_set_time = sso[month][int(day)-1]
-      sun_set = pd.to_datetime(f"{single_date.strftime('%Y-%m-%d')} {sun_set_time}")
-      fig = add_sun_annot(fig, sun_set)
+    winds = []
 
+    for idx,t in enumerate(times):
+        winds.append({
+            'time': t.text,
+            'speed': wind_speeds[idx].text,
+            'direction': wind_dir[idx].text
+        })
+    # print(len(times))
+    # print(len(wind_speeds))
+    # print(len(wind_dir))
+    # for t in times:
+    #     print(t.text)
+
+    for w in winds[:24]:
+        time = w['time']
+        wind = f"{w['speed']}kt<br>{deg_to_compass(w['direction'])}"
+        fig.add_annotation(x=time, y=5, text=wind, showarrow=False)
+
+
+    # for d in forecast_hourly.json()['properties']['windSpeed']['values'][:-12]:
+    #     valid_time = d['validTime'].split('/')
+    #     time = pd.to_datetime(valid_time[0])
+    #     valid_hours = int(valid_time[1].removeprefix("PT").removesuffix("H"))
+    #     wind = f"{km_to_kn(d['value'])} kt"
+
+    #     if valid_hours > 1:
+    #         fig.add_annotation(x=time, y=5, text=wind, showarrow=False)
+    #         # while valid_hours > 1:
+    #         #     time = time + timedelta(hours=1)
+    #         #     fig.add_annotation(x=time, y=5, text=wind, showarrow=False)
+    #         #     valid_hours = valid_hours - 1
+    #     else:
+    #         fig.add_annotation(x=time, y=5, text=wind, showarrow=False)
+
+    # for d in forecast_hourly.json()['properties']['windDirection']['values'][:-12]:
+    #     valid_time = d['validTime'].split('/')
+    #     time = pd.to_datetime(valid_time[0])
+    #     valid_hours = int(valid_time[1].removeprefix("PT").removesuffix("H"))
+    #     wind = deg_to_compass(d['value'])
+
+    #     if valid_hours > 1:
+    #         fig.add_annotation(x=time, y=4.9, text=wind, showarrow=False)
+    #         # while valid_hours > 1:
+    #         #     time = time + timedelta(hours=1)
+    #         #     fig.add_annotation(x=time, y=4.9, text=wind, showarrow=False)
+    #         #     valid_hours = valid_hours - 1
+    #     else:
+    #         fig.add_annotation(x=time, y=4.9, text=wind, showarrow=False)
 
     fig.update_traces(textposition=improve_text_position(dc['Time']))
 

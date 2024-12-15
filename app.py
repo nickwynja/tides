9fa7e746f6df1b669ba342c56478db2dadb2c19d
Nick Wynja <nick@nickwynja.com>
2024-12-14 19:53:25 -0500

    waves

--- app.py
+++ app.py
@@ -905,7 +905,15 @@ def tides():
             cond = f"{cond}<br>{round(d['wind_speed_10m'])}-{round(d['wind_gusts_10m'])} kt"
         else:
             cond = f"{cond}<br>{round(d['wind_speed_10m'])} kt"
-        cond = f"{cond}<br>{round(d['wave_height'], 1)}' @ {round(d['wave_period'])}s"
+
+        w = round(d['wave_height'])
+
+        if w > 2:
+            wave_cond = f"{w}' @ {round(d['wave_period'])}s"
+        else:
+            wave_cond = f"{w}'"
+
+        cond = f"{cond}<br>{wave_cond}"
         cond = f"{cond}<br>{round(d['temperature_2m'])}&deg;F"
         wind_annots = wind_annots + [dict(x=t, yref="paper", y=1, yshift=55, text=cond, showarrow=False, name="wind")]
 
